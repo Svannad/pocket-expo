4eb38a291273eb87a79cfe6b2f4181fcb733d1b2
Svannad <svannadam@gmail.com>
2025-05-05 17:40:02 +0200

    Grid Toggle

--- Pocket Expo/Assets/Scripts/BuildingManager.cs
+++ Pocket Expo/Assets/Scripts/BuildingManager.cs
@@ -10,12 +10,23 @@ public class BuildingManager : MonoBehaviour
     private Vector3 pos;
     private RaycastHit hit;
     [SerializeField] private LayerMask layerMask;
+    public float gridSize;
+    bool gridOn = true;
+    [SerializeField] private Toggle gridToggle;
 
     void Update()
     {
         if (pendingObject != null)
         {
-            pendingObject.transform.position = pos;
+            if (gridOn)
+            {
+                pendingObject.transform.position = new Vector3(
+                    RoundToNearestGrid(pos.x),
+                    RoundToNearestGrid(pos.y),
+                    RoundToNearestGrid(pos.z)
+                );
+            }
+            else { pendingObject.transform.position = pos; }
 
             if (Input.GetMouseButtonDown(0))
             {
@@ -43,5 +54,29 @@ public class BuildingManager : MonoBehaviour
     public void SelectingObject(int index)
     {
         pendingObject = Instantiate(objects[index], pos, transform.rotation);
+    }
+
+    public void GridToggle()
+    {
+        if (gridToggle.isOn)
+        {
+            gridOn = true;
+        }
+        else
+        {
+            gridOn = false;
+        }
+    }
+
+    float RoundToNearestGrid(float pos)
+    {
+        float xDiff = pos % gridSize;
+        pos -= xDiff;
+
+        if (xDiff > (gridSize / 2))
+        {
+            pos += gridSize;
+        }
+        return pos;
     }
 }
