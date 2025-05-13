90fb2cd05c9597f847e6732f0b7d803691fae27d
Svannad <svannadam@gmail.com>
2025-05-13 23:32:22 +0200

    No Grid

--- Pocket Expo/Assets/Scripts/BuildingManager.cs
+++ Pocket Expo/Assets/Scripts/BuildingManager.cs
@@ -10,41 +10,23 @@ public class BuildingManager : MonoBehaviour
     private Vector3 pos;
     private RaycastHit hit;
     [SerializeField] private LayerMask layerMask;
-
     public float rotateAmount;
-    public float gridSize;
-    bool gridOn = true;
-    [SerializeField] private Toggle gridToggle;
-
     public float scaleStep = 0.1f;
     public float minScale = 0.3f;
     public float maxScale = 3f;
+
 
     void Update()
     {
         if (pendingObject != null)
         {
-            // Set object position
-            if (gridOn)
-            {
-                pendingObject.transform.position = new Vector3(
-                    RoundToNearestGrid(pos.x),
-                    RoundToNearestGrid(pos.y),
-                    RoundToNearestGrid(pos.z)
-                );
-            }
-            else
-            {
-                pendingObject.transform.position = pos;
-            }
+            pendingObject.transform.position = pos;
 
-            // Place on left click
             if (Input.GetMouseButtonDown(0))
             {
                 PlaceObject();
             }
 
-            // Rotate on R
             if (Input.GetKeyDown(KeyCode.R))
             {
                 RotateObject();
@@ -87,24 +69,6 @@ public class BuildingManager : MonoBehaviour
     {
         pendingObject.transform.Rotate(Vector3.up, rotateAmount);
     }
-
-    public void GridToggle()
-    {
-        gridOn = gridToggle.isOn;
-    }
-
-    float RoundToNearestGrid(float pos)
-    {
-        float xDiff = pos % gridSize;
-        pos -= xDiff;
-
-        if (xDiff > (gridSize / 2))
-        {
-            pos += gridSize;
-        }
-        return pos;
-    }
-
     private void ScaleObject(float amount)
     {
         Vector3 currentScale = pendingObject.transform.localScale;
