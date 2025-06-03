492ed7c754461aa9b0445a7fa2aca37d66f1a1f6
Svannad <svannadam@gmail.com>
2025-06-03 11:46:36 +0200

    New Player Guide

---
+++ Pocket Expo/Assets/Scripts/OnboardScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class OnboardScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
